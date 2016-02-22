9ce435d8131b910f6768a001a836d672f5eaec12
Brett Bukowski <brett.bukowski@gmail.com>
2016-02-21 21:40:26 -0800

    styling

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
  */
 
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -26,6 +26,25 @@
   <div class="site-inner">
     <a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'epic' ); ?></a>
 
+    <?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
+      <div id="site-header-menu" class="site-header-menu">
+        <?php if ( has_nav_menu( 'primary' ) ) : ?>
+          <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'epic' ); ?>">
+            <div>
+              <a href="/"><span class="screen-reader-text">Epic</span></a>
+            </div>
+            <?php
+              wp_nav_menu( array(
+                'theme_location' => 'primary',
+                'menu_class'     => 'primary-menu',
+               ) );
+            ?>
+          </nav><!-- .main-navigation -->
+        <?php endif; ?>
+      </div><!-- .site-header-menu -->
+    <?php endif; ?>
+  </div><!-- .site-header-main -->
+
     <header id="masthead" class="site-header" role="banner">
       <div class="site-header-main">
         <div class="site-branding">
@@ -33,31 +52,9 @@
             <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
           <?php else : ?>
             <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-          <?php endif;
-
-          $description = get_bloginfo( 'description', 'display' );
-          if ( $description || is_customize_preview() ) : ?>
-            <p class="site-description"><?php echo $description; ?></p>
           <?php endif; ?>
         </div><!-- .site-branding -->
 
-        <?php if ( has_nav_menu( 'primary' ) || has_nav_menu( 'social' ) ) : ?>
-          <button id="menu-toggle" class="menu-toggle"><?php _e( 'Menu', 'epic' ); ?></button>
-
-          <div id="site-header-menu" class="site-header-menu">
-            <?php if ( has_nav_menu( 'primary' ) ) : ?>
-              <nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'epic' ); ?>">
-                <?php
-                  wp_nav_menu( array(
-                    'theme_location' => 'primary',
-                    'menu_class'     => 'primary-menu',
-                   ) );
-                ?>
-              </nav><!-- .main-navigation -->
-            <?php endif; ?>
-          </div><!-- .site-header-menu -->
-        <?php endif; ?>
-      </div><!-- .site-header-main -->
 
       <?php if ( get_header_image() ) : ?>
         <?php
