d31c56c301d985164fc8f1dc1b7f4fbda18db299
Brett Bukowski <brett.bukowski@gmail.com>
2016-07-03 18:21:12 -0700

    always link thumbnail images

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -123,21 +123,12 @@ function epic_post_thumbnail() {
   if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
     return;
   }
-
-  if ( is_singular() ) :
-  ?>
-
-  <div class="post-thumbnail">
-    <?php the_post_thumbnail(); ?>
-  </div><!-- .post-thumbnail -->
-
-  <?php else : ?>
+?>
 
   <a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true">
-    <?php the_post_thumbnail( 'post-thumbnail', array( 'alt' => the_title_attribute( 'echo=0' ) ) ); ?>
+    <?php the_post_thumbnail( 'post-thumbnail', array( 'alt' => the_title_attribute( 'echo=0' ), 'class' => 'img-fluid' ) ); ?>
   </a>
-
-  <?php endif; // End is_singular()
+<?php
 }
 endif;
 
