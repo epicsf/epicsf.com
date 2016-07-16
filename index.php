628d5f71ef6c672752ef5e29aa2cf0e4af0b3916
Brett Bukowski <brett.bukowski@gmail.com>
2016-07-15 22:04:14 -0700

    get rid of horizontal scrollbars on homepage

--- index.php
+++ index.php
@@ -23,107 +23,108 @@ get_header(); ?>
 
       <?php if ( is_home() ) : ?>
         <header class="home-header"></header>
-        <div class="row home-featured">
-            <?php
-              $featured = epic_get_featured_post();
-              if ( $featured ) { ?>
-          <div class="col-md-10 col-md-offset-2 col-xs-12">
-            <h1 class="variation"><?php echo get_the_title($featured); ?></h1>
-            <div class="body2"><?php get_the_subtitle($featured) ?></div>
-            <div class="spacer-30"></div>
-          </div>
-          <div class="col-md-5 col-md-offset-2 col-xs-12">
-            <?php echo $featured->post_content; ?>
-          </div>
-            <?php } ?>
-          <div class="col-md-4 featured-messages col-xs-12">
-            <?php
-              $featured_messages = epic_get_featured_messages_post();
-              if ( $featured_messages ) { ?>
-              <?php echo $featured_messages->post_content; ?>
+        <div class="container-fluid">
+          <div class="row home-featured">
+              <?php
+                $featured = epic_get_featured_post();
+                if ( $featured ) { ?>
+            <div class="col-md-10 col-md-offset-2 col-xs-12">
+              <h1 class="variation"><?php echo get_the_title($featured); ?></h1>
+              <div class="body2"><?php get_the_subtitle($featured) ?></div>
+              <div class="spacer-30"></div>
+            </div>
+            <div class="col-md-5 col-md-offset-2 col-xs-12">
+              <?php echo $featured->post_content; ?>
+            </div>
               <?php } ?>
-          </div>
-        </div>
-        <div class="row hidden-xs-down">
-          <div class="col-md-11 col-md-offset-1 col-xs-12">
-            <div class="carousel slide" id="carousel" data-ride="carousel">
-              <div class="carousel-inner">
+            <div class="col-md-4 featured-messages col-xs-12">
               <?php
-                $all_posts = epic_get_events();
-                $i = 0;
-                $posts = array_slice($all_posts, $i, 3);
-                while(count($posts)): ?>
-                <div class="carousel-item <?php echo $i === 0 ? 'active' : 'next'; ?>">
-                  <div class="row">
-                <?php foreach($posts as $post): ?>
-                  <div class="col-md-4 col-xs-12">
-                    <?php epic_post_thumbnail() ?>
-                  </div>
-                <?php endforeach; ?>
-                  </div> <!-- row -->
-                </div> <!-- item -->
+                $featured_messages = epic_get_featured_messages_post();
+                if ( $featured_messages ) { ?>
+                <?php echo $featured_messages->post_content; ?>
+                <?php } ?>
+            </div>
+          </div> <!-- .home-featured -->
+          <div class="row hidden-xs-down">
+            <div class="col-md-11 col-md-offset-1 col-xs-12">
+              <div class="carousel slide" id="carousel" data-ride="carousel">
+                <div class="carousel-inner">
                 <?php
-                  $i+= 3;
+                  $all_posts = epic_get_events();
+                  $i = 0;
                   $posts = array_slice($all_posts, $i, 3);
-                endwhile;
-                ?>
-              </div> <!-- inner -->
-            </div> <!-- carousel.slide -->
-          </div> <!-- col -->
-        </div> <!-- row -->
-        <?php if (count($all_posts) > 3): ?>
-        <div class="row hidden-xs-down">
-          <div class="col-md-10 col-md-offset-1 col-xs-12">
-            <div class="carousel-controls text-md-right text-xs-right">
-              <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
-                <i class="fa fa-angle-left"></i>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
-                <i class="fa fa-angle-right"></i>
-                <span class="sr-only">Next</span>
-              </a>
+                  while(count($posts)): ?>
+                  <div class="carousel-item <?php echo $i === 0 ? 'active' : 'next'; ?>">
+                    <div class="row">
+                  <?php foreach($posts as $post): ?>
+                    <div class="col-md-4 col-xs-12">
+                      <?php epic_post_thumbnail() ?>
+                    </div>
+                  <?php endforeach; ?>
+                    </div> <!-- row -->
+                  </div> <!-- item -->
+                  <?php
+                    $i+= 3;
+                    $posts = array_slice($all_posts, $i, 3);
+                  endwhile;
+                  ?>
+                </div> <!-- inner -->
+              </div> <!-- carousel.slide -->
+            </div> <!-- col -->
+          </div> <!-- row -->
+          <?php if (count($all_posts) > 3): ?>
+          <div class="row hidden-xs-down">
+            <div class="col-md-10 col-md-offset-1 col-xs-12">
+              <div class="carousel-controls text-md-right text-xs-right">
+                <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+                  <i class="fa fa-angle-left"></i>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+                  <i class="fa fa-angle-right"></i>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
             </div>
-          </div>
-        </div> <!-- row -->
-        <?php endif; ?>
-
-        <div class="row hidden-sm-up">
-          <div class="col-xs-12">
-            <div class="carousel slide" id="carousel-mobile" data-ride="carousel">
-              <div class="carousel-inner">
-              <?php
-                $all_posts = epic_get_events();
-                $i = 0; ?>
-                <?php foreach($all_posts as $post): ?>
-                <div class="carousel-item <?php echo $i === 0 ? 'active' : 'next'; ?>">
-                  <div class="row">
-                  <div class="col-xs-12">
-                    <?php epic_post_thumbnail() ?>
-                  </div>
-                  </div> <!-- row -->
-                </div> <!-- item -->
-                <?php $i++; ?>
-                <?php endforeach; ?>
-              </div> <!-- inner -->
-            </div> <!-- carousel.slide -->
-          </div> <!-- col -->
-        </div> <!-- row -->
-        <div class="row hidden-sm-up">
-          <div class="col-md-10 col-md-offset-1 col-xs-12">
-            <div class="carousel-controls text-md-right text-xs-right">
-              <a class="left carousel-control" href="#carousel-mobile" role="button" data-slide="prev">
-                <i class="fa fa-angle-left"></i>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="right carousel-control" href="#carousel-mobile" role="button" data-slide="next">
-                <i class="fa fa-angle-right"></i>
-                <span class="sr-only">Next</span>
-              </a>
+          </div> <!-- row -->
+          <?php endif; ?>
+          <div class="row hidden-sm-up">
+            <div class="col-xs-12">
+              <div class="carousel slide" id="carousel-mobile" data-ride="carousel">
+                <div class="carousel-inner">
+                <?php
+                  $all_posts = epic_get_events();
+                  $i = 0; ?>
+                  <?php foreach($all_posts as $post): ?>
+                  <div class="carousel-item <?php echo $i === 0 ? 'active' : 'next'; ?>">
+                    <div class="row">
+                    <div class="col-xs-12">
+                      <?php epic_post_thumbnail() ?>
+                    </div>
+                    </div> <!-- row -->
+                  </div> <!-- item -->
+                  <?php $i++; ?>
+                  <?php endforeach; ?>
+                </div> <!-- inner -->
+              </div> <!-- carousel.slide -->
+            </div> <!-- col -->
+          </div> <!-- row -->
+          <div class="row hidden-sm-up">
+            <div class="col-md-10 col-md-offset-1 col-xs-12">
+              <div class="carousel-controls text-md-right text-xs-right">
+                <a class="left carousel-control" href="#carousel-mobile" role="button" data-slide="prev">
+                  <i class="fa fa-angle-left"></i>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#carousel-mobile" role="button" data-slide="next">
+                  <i class="fa fa-angle-right"></i>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
             </div>
-          </div>
-        </div> <!-- row -->
-        <div class="spacer-80"></div>
+          </div> <!-- row -->
+          <div class="spacer-80"></div>
+        </div>
       <?php endif; ?>
 
       <?php
@@ -145,10 +146,13 @@ get_header(); ?>
     <?php
       if ( is_home() ) {
         $page = get_page_by_title('Home');
-        if ( $page ) {
-          echo $page->post_content;
-        }
-      } else {
+        if ( $page ) { ?>
+        <div class="container-fluid">
+          <?php echo $page->post_content; ?>
+        </div>
+        <?php }
+      }
+      else {
         $page = strtolower( the_title(null, null, false) );
         if ( $page === 'events' ) {
           $posts = epic_get_events();
