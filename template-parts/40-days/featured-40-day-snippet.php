c86253aa088e8140faf03cd9ee6a92f51110377e
Brett Bukowski <brett.bukowski@gmail.com>
2018-02-08 07:33:07 -0800

    swap title/subtitle

--- template-parts/40-days/featured-40-day-snippet.php
+++ template-parts/40-days/featured-40-day-snippet.php
@@ -28,9 +28,8 @@
     <div class="hidden-sm-up event-snippet col-xs-12">
       <div class="spacer-60"></div>
       <div><?php epic_40_day_prayer_post_thumbnail() ?></div>
-      <div class="spacer-20"></div>
-      <div class="title-3"><?php the_title() ?></div>
       <div class="title-2"><?php the_subtitle() ?></div>
+      <div class="title-3"><?php the_title() ?></div>
       <div class="forty-day-content" hidden>
         <p class="forty-day-entry-content body2"><?php $post = get_post(); echo $post->post_content; ?></p>
       </div>
