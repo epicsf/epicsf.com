7ad4e9063adb42f0dea06a4fe4d8edeab3cf0082
Brett Bukowski <brett.bukowski@gmail.com>
2019-01-24 20:43:44 -0800

    add wps subtitle

--- template-parts/link-tree/link-tree.php
+++ template-parts/link-tree/link-tree.php
@@ -3,7 +3,7 @@
 <div class="col-md-6 col-md-offset-3 link-tree col-xs-12">
 
 <?foreach ( get_post_custom() as $key => $value ) { ?>
-  <?php if ( strpos($key, '_') === 0 ) { continue; } ?>
+  <?php if ( strpos($key, '_') === 0 || strpos($key, 'wps_subtitle') === 0 ) { continue; } ?>
   <a class="btn btn-block" href="<?php echo trim( $value[0] ); ?>">
     <?php echo $key; ?>
   </a>
