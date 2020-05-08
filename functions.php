60d3572e8be77cc12f904ba83646e8e8657e5dbf
Brett Bukowski <brett.bukowski@gmail.com>
2020-05-08 16:22:26 -0700

    Header and footer redesign

--- functions.php
+++ functions.php
@@ -16,6 +16,10 @@ if (!function_exists('epic_setup')) {
       'footer-about-us'      => 'Footer About Us Menu',
       'footer-media'         => 'Footer Media Menu',
       'footer-get-connected' => 'Footer Get Connected Menu',
+      'footer-watch'         => 'Footer Watch Menu',
+      'footer-connect'       => 'Footer Connect Menu',
+      'footer-give'          => 'Footer Give Menu',
+      'footer-social'        => 'Footer Social',
     ));
 
     /*
@@ -69,7 +73,7 @@ if (!function_exists('epic_fonts_url')) {
     $fonts_url = '';
     $fonts = array(
       'Cardo:400,400i',
-      'Lato:300,400,700',
+      'Lato:300,400,700,900',
     );
 
     $fonts_url = add_query_arg(array(
@@ -248,7 +252,7 @@ function epic_scripts() {
 
   wp_enqueue_style(
     'epic-font-icons',
-    'https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
     array(),
     null
   );
@@ -259,7 +263,7 @@ function epic_scripts() {
     get_stylesheet_uri(),
     null,
     // Increment this to match the theme version to bust the cache.
-    '1.0.38'
+    '1.0.40'
   );
 
 }
